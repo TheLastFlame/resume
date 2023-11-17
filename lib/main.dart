82d5ddfcd63488cbad194ec31cb155f14b88d067
TheLastFlame <TheLastSight@Mail.Ru>
2023-11-17 22:16:51 +1000

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:resume/pages/home/home.dart';
+import 'package:resume/navigation/router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Homepage());
+    return MaterialApp.router(
+      darkTheme: ThemeData.dark(),
+      routerConfig: router,
+    );
   }
 }
---
+++ lib/navigation/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:resume/navigation/swipeable_page.dart';
+import 'package:resume/pages/home/home.dart';
+
+final rootNavigationKey = GlobalKey<NavigatorState>();
+
+final router = GoRouter(
+  navigatorKey: rootNavigationKey,
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/home',
+      pageBuilder: (context, state) => MaterialPage(child: Homepage()),
+    ),
+    GoRoute(
+      path: '/profile',
+      pageBuilder: (context, state) => SwipeablePage(child: Placeholder()),
+    ),
+  ],
+);
